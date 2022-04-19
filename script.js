6d081fbe02f6eaf604e6d474cc2916417da6902d
Damar <damarsepti627@gmail.com>
2022-04-20 06:15:25 +0700

    Penyempurnaan slider

---
+++ script.js
@@ -0,0 +1,48 @@
+/* function radioChecked() {
+    manualBtn = document.getElementsByClassName("manual-button");
+    for (let i = 1; i < 6; i++) {
+        if (document.getElementById("radio" + i).checked) {
+            if (i == 1) {
+                document.querySelector(".first").style.marginLeft = "0";
+                manualBtn[0].style.backgroundColor = "#00bcd4";
+                for (let j = 0; j < 5; j++) {
+                    if (j != 0) {
+                        manualBtn[j].style.backgroundColor = "transparent";
+                    }
+                }
+            } else if (i == 2) {
+                document.querySelector(".first").style.marginLeft = "-20%";
+                manualBtn[1].style.backgroundColor = "#00bcd4";
+                for (let j = 0; j < 5; j++) {
+                    if (j != 1) {
+                        manualBtn[j].style.backgroundColor = "transparent";
+                    }
+                }
+            } else if (i == 3) {
+                document.querySelector(".first").style.marginLeft = "-40%";
+                manualBtn[2].style.backgroundColor = "#00bcd4";
+                for (let j = 0; j < 5; j++) {
+                    if (j != 2) {
+                        manualBtn[j].style.backgroundColor = "transparent";
+                    }
+                }
+            } else if (i == 4) {
+                document.querySelector(".first").style.marginLeft = "-60%";
+                manualBtn[3].style.backgroundColor = "#00bcd4";
+                for (let j = 0; j < 5; j++) {
+                    if (j != 3) {
+                        manualBtn[j].style.backgroundColor = "transparent";
+                    }
+                }
+            } else if (i == 5) {
+                document.querySelector(".first").style.marginLeft = "-80%";
+                manualBtn[4].style.backgroundColor = "#00bcd4";
+                for (let j = 0; j < 5; j++) {
+                    if (j != 4) {
+                        manualBtn[j].style.backgroundColor = "transparent";
+                    }
+                }
+            }
+        }
+    }
+} */
