2c840faf91d92a53c7abb856ed28c56c0cb3fcde
Damar <damarsepti627@gmail.com>
2022-04-20 09:31:35 +0700

    Fungsi untuk next dan previous slide

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
-/* function radioChecked() {
-    manualBtn = document.getElementsByClassName("manual-button");
-    for (let i = 1; i < 6; i++) {
-        if (document.getElementById("radio" + i).checked) {
-            if (i == 1) {
-                document.querySelector(".first").style.marginLeft = "0";
-                manualBtn[0].style.backgroundColor = "#00bcd4";
-                for (let j = 0; j < 5; j++) {
-                    if (j != 0) {
-                        manualBtn[j].style.backgroundColor = "transparent";
-                    }
-                }
-            } else if (i == 2) {
-                document.querySelector(".first").style.marginLeft = "-20%";
-                manualBtn[1].style.backgroundColor = "#00bcd4";
-                for (let j = 0; j < 5; j++) {
-                    if (j != 1) {
-                        manualBtn[j].style.backgroundColor = "transparent";
-                    }
-                }
-            } else if (i == 3) {
-                document.querySelector(".first").style.marginLeft = "-40%";
-                manualBtn[2].style.backgroundColor = "#00bcd4";
-                for (let j = 0; j < 5; j++) {
-                    if (j != 2) {
-                        manualBtn[j].style.backgroundColor = "transparent";
-                    }
-                }
-            } else if (i == 4) {
-                document.querySelector(".first").style.marginLeft = "-60%";
-                manualBtn[3].style.backgroundColor = "#00bcd4";
-                for (let j = 0; j < 5; j++) {
-                    if (j != 3) {
-                        manualBtn[j].style.backgroundColor = "transparent";
-                    }
-                }
-            } else if (i == 5) {
-                document.querySelector(".first").style.marginLeft = "-80%";
-                manualBtn[4].style.backgroundColor = "#00bcd4";
-                for (let j = 0; j < 5; j++) {
-                    if (j != 4) {
-                        manualBtn[j].style.backgroundColor = "transparent";
-                    }
-                }
-            }
-        }
+var counter = 2;
+
+function slideCounter(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 5){
+        counter = 1;
     }
-} */
+}
+
+var timer = setInterval(slideCounter, 5000);
+
+function resetTimer(){
+    clearInterval(timer);
+    timer = setInterval(slideCounter, 5000);
+}
+
+function nextSlide() {
+    if(counter == 5){
+        counter = 1;
+    }
+    else{
+        counter++;
+    }
+    document.getElementById('radio' + counter).checked = true;
+}
+
+function previousSlide() {
+    if(counter == 1){
+        counter = 5;
+    }
+    else{
+        counter--;
+    }
+    document.getElementById('radio' + counter).checked = true;
+}
